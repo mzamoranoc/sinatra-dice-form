46310278ace39891c2ee61b32b0422ef0ba3e18d
mzamoranoc <mzamoran@chicagobooth.edu>
2024-10-23 07:49:11 +0000

    error check

--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
-require "sinatra"
-require "sinatra/reloader"
+require 'sinatra'
+require 'sinatra/reloader' if development?
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+# Route to display the form on the homepage
+get '/' do
+  erb :index
 end
+
+# Route to process the roll, taking query parameters 'dice' and 'sides'
+get '/process_roll' do
+  # Fetch number of dice and sides from query string
+  number_of_dice = params[:dice].to_i
+  sides_per_die = params[:sides].to_i
+
+  # Generate the dice rolls
+  @rolls = (1..number_of_dice).map { rand(1..sides_per_die) }
+
+  erb :result
+end
+
